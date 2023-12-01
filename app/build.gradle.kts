cbcba8e2c78df0ee3ea167b3002323f5863c99ef
RastislavKish <rastislav.kish@protonmail.com>
2023-12-01 17:09:07 +0100

    Upgrade application project template
    Generate a new project template for the app using the latest preview release of Android Studio Iguana. The new project template is targeting Android SDK 34, uses Kotlin DSL and bumps versions of several dependencies such as the Android gradle plugin, Gradle itself with its wrapper, but also CameraX, which required targeting API 34 in its newer versions.

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.jetbrainsKotlinSerialization)
+}
+
+android {
+    namespace = "com.rastislavkish.vscan"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.rastislavkish.vscan"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 12
+        versionName = "0.1.2"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+implementation(libs.rtk.kotlin.android)
+
+implementation(libs.camera.core)
+implementation(libs.camera.lifecycle)
+implementation(libs.camera.camera2)
+implementation(libs.camera.view)
+implementation(libs.ktor.client.core)
+implementation(libs.ktor.client.okhttp)
+implementation(libs.kotlinx.coroutines.core)
+implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
