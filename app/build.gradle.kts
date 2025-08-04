9f00e1c8e7e6612e2b304a1f309dbd0d2faa8259
RastislavKish <rastislav.kish@protonmail.com>
2025-08-04 14:45:57 +0200

    Bump version to 0.2.2
    Update the Fastlane metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rastislavkish.vscan"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.2.1"
+        versionCode = 22
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
