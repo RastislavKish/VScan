fa5451417b62e357c8757f604deb3692e4d700fc
RastislavKish <rastislav.kish@protonmail.com>
2026-06-19 19:19:54 +0200

    Bump version to v0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rastislavkish.vscan"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.2.3"
+        versionCode = 24
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
